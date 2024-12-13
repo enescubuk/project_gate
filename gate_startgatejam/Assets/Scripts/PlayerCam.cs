a6f89259335ce8a504a91486339c2aabb2cf0c73
Enes <enescubuk10@gmail.com>
2024-12-14 00:41:45 +0300

    playermovement and cammovement

---
+++ gate_startgatejam/Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    [SerializeField] private float SensX;
+    [SerializeField] private float SensY;
+
+    [SerializeField] private Transform orientation;
+
+    private float xRotation;
+    private float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * SensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * SensY;
+    
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
