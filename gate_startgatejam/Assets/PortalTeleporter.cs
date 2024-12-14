01004d7cb732badeff483bb03a002deed13bd56f
Mert Ates Topuz <m.atestopuz@gmail.com>
2024-12-14 12:55:56 +0300

    Mert Commit 6
    Koridor paradoxu yapıldı

---
+++ gate_startgatejam/Assets/PortalTeleporter.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour
+{
+    public Transform player;
+    public Transform reciver;
+    private bool playerIsOverlapping = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if (dotProduct < 0f)
+            {
+                float rotationDiff = Quaternion.Angle(transform.rotation, reciver.rotation);
+                //rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = reciver.position + positionOffset;
+            print("yes");
+
+
+            }
+        }
+    }
+}
